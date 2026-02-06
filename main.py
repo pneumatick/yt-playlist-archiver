f87e4976a3cdb0d6667b36779a477bb57f6f78e1
pneumatick <jhwwefan@gmail.com>
2026-02-06 10:31:21 -0500

    Remove some comments

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import csv
 import pandas as pd
 
 from google.oauth2.credentials import Credentials
+from google.auth.transport.requests import Request
 import google_auth_oauthlib.flow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
@@ -18,12 +19,6 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 # client_secret. You can acquire an OAuth 2.0 client ID and client secret from
 # the {{ Google Cloud Console }} at
 # {{ https://cloud.google.com/console }}.
-# Please ensure that you have enabled the YouTube Data API for your project.
-# For more information about using OAuth2 to access the YouTube Data API, see:
-#   https://developers.google.com/youtube/v3/guides/authentication
-# For more information about the client_secrets.json file format, see:
-#   https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
-
 CLIENT_SECRETS_FILE = 'client_secret.json'
 
 # This OAuth 2.0 access scope allows for full read/write access to the
@@ -65,6 +60,7 @@ def get_authenticated_service():
 
     return build(API_SERVICE_NAME, API_VERSION, credentials = credentials)
 
+'''
 # Get the API key from the specified text file
 def get_api_key(path="secrets.txt"):
     try:
@@ -74,6 +70,7 @@ def get_api_key(path="secrets.txt"):
     except Exception as e:
         print(f"An error occured while attempting to get API key: {e}")
         return None
+'''
 
 # Retrieve n items (50 max) from a playlist
 def get_playlist_page(playlist_id, n_items = 50, next_page = None):
